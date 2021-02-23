c21716b64a36ff05a971346f54d14b4958f788ec
Joao Almeida <joaoalmeida11@ua.pt>
2021-02-23 10:20:32 +0000

    changed interface methods to static

--- index.php
+++ index.php
@@ -1,21 +1,28 @@
 <?php
 
-interface iValueValidation{
-    public function isPositiveNumber($newValue);
+interface iNumberValidation{
+    public static function isNumber($newValue);
+    public static function isPositiveNumber($newValue);
+}
+interface iColorValidation{
+    public static function isColor($newValue);
 }
 interface iFigureOperations{
     public function perimeter();
     public function area();
 }
 
-final class Ponto implements iValueValidation{
+final class Ponto implements iNumberValidation{
     private $x;
     private $y;
 
     private function __construct($x, $y){
-        if($this->isPositiveNumber($x) && $this->isPositiveNumber($y)){
+        if(Ponto::isNumber($x) && Ponto::isNumber($y)){
             $this->x = $x;
             $this->y = $y;
+        }
+        else{
+            throw new Exception("can not create ponto");
         }
     }
 
@@ -31,15 +38,11 @@ final class Ponto implements iValueValidation{
     protected function setY($newValue){
         $this->y = $newValue;
     }
-    public function isPositiveNumber($newValue){
+
+    public static function isNumber($newValue){
         if(isset($newValue)){
             if(is_numeric($newValue)){
-                if($newValue>0){
-                    return $newValue;
-                }
-                else{
-                    throw new Exception("value is not a positive number");
-                }
+                return $newValue;
             }
             else{
                 throw new Exception("value is not numeric");
@@ -49,15 +52,51 @@ final class Ponto implements iValueValidation{
             throw new Exception("value is null");
         }
     }
+    public static function isPositiveNumber($newValue){
+        if(Ponto::isNumber($newValue)){
+             if($newValue>0){
+                return $newValue;
+            }
+            else{                
+                throw new Exception("value is not a positive number");
+             }
+        }
+    }
 }
 
-abstract class Figures2D{
+abstract class Figures2D implements iColorValidation{
     private $color;
+
+    private function __construct($color){
+        if(Figures2D::isColor($color)){
+            $this->color = $color;
+        }
+    }
+
     protected function getColor(){
         return $this->color;
     }
     protected function setColor($newValue){
         $this->color= $newValue;
+    }
+
+    public static function isColor($newValue){
+        if(isset($newValue)){
+            if(is_string($newValue)){
+                if(preg_match("/[^a-zA-Z]/",$newValue)){
+                    return $newValue;
+                }
+                else{
+                    throw new Exception("color is not only letters");
+                }
+            }
+            else{
+                throw new Exception("color is not string");
+            }
+        }
+        else{
+            throw new Exception("color is null");
+        }
     }
 }
 
@@ -65,5 +104,9 @@ class Circles extends Figures2D{
     private $ponto1;
     private $radius;
 
+    private function __construct($x, $y, $radius){
+        $this->ponto1 = new Ponto($$x, $y);
+
+    }
 
 }
